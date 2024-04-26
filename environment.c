611096aa6dbfaed73984af3c00f38772059e43d8
DeanSingular <Dmabuela@singular.co.za>
2024-04-26 21:00:59 +0000

    hols

--- environment.c
+++ environment.c
@@ -1,98 +1,42 @@
-#include "shell.h"
+#include "main.h"
 
 /**
- * env - sets a linked list for the enviornment
- * Return: head of the linked list
-*/
-path_t *_env(void)
+ * _getenv - get env variables
+ * @env_var: env variable
+ * Return: env variable result, its content
+ */
+
+char *_getenv(char *env_var)
 {
-	path_t *head;
-	path_t *house;
-	int count = 0;
+	int i = 0, j;
+	int status;
 
-	house = malloc(sizeof(path_t)); /*linked list*/
-	if (!house)
+	while (environ[i])
 	{
-		free(house);
-		return (NULL);
-	}
-	head = house; /*pointer to beginning of list */
-	while (environ[count]) /*enviorn makes user enviornment*/
-	{
-		house->ptr = _strdup(environ[count]);/*dup variable into list*/
-		if (environ[count + 1] != NULL)
+		status = 1;
+
+		for (j = 0; environ[i][j] != '='; j++)
 		{
-			house->next = malloc(sizeof(path_t));
-			house = house->next;
+			if (environ[i][j] != env_var[j])
+				status = 0;
 		}
-		count++;
-	}
-	house->next = NULL;
-	return (head);
-}
-
-/**
- * env_find - searches for a match in the enviornment list
- * @var: argument passed in
- * Return: pointer to matched variable
-*/
-char *env_find(char *var)
-{
-	char *same, *store;
-	path_t *node;
-	unsigned int count;
-
-	same = NULL;
-	node = enviornment; /*runs enviornment list (man env)*/
-
-	while (node)
-	{
-		count = 0;
-	/*while there are arguments*/
-		while (var[count])
-		{
-			if (var[count] != node->ptr[count])
-				break;
-			if (var[count + 1] == '\0' && node->ptr[count + 1] == '=')
-			same = node->ptr;
-			count++;
-		}
-		/** once we find a match exit loop*/
-		if (same)
+		if (status == 1)
 			break;
-		node = node->next;
+		i++;
 	}
-	store = malloc(sizeof(char) * (_strlen(same) + 1));
-	if (!store)
-		/*free (store);*/
-		return (NULL);
-	_strcpy(store, same);
-	return (store);
+	return (&environ[i][j + 1]);
 }
-/**
- * env_match - finds the matching enviornment with the argument passed
- * @input: argument passed
- * Return: 0 on success, -1 if no match
-*/
-int env_match(char *input)
-{
-	path_t *peter;
-	int count = 0, count_two = 0;
 
-	peter = enviornment; /*setting enviornment*/
-	while (peter)
+/**
+ * _env - prints environment
+*/
+void _env(void)
+{
+	int i = 0;
+
+	while (environ[i])
 	{
-		count = 0;
-		while (input[count] == peter->ptr[count])
-		{
-			count++;
-			if (input[count + 1] == '\0' && (peter->ptr)[count + 1] == '=')
-				return (count_two);
-		}
-		count_two++;
-		peter = peter->next;
-		if (peter == NULL)
-			return (-1);
+		printf("%s\n", environ[i]);
+		i++;
 	}
-	return (count_two);
 }
--- memory.c
+++ memory.c
@@ -1,91 +1,27 @@
-#include "shell.h"
+#include "main.h"
 
 /**
- * _realloc - reallocates memory block
- * @ptr: pointer to the previous memory
- * @old_size: the old size
- * @new_size: the new size
- *
- * Return: a pointer to the newly allocated memory
+ * _calloc - function that allocates memory for an array, using malloc
+ * @nmemb: array
+ * @size: size
+ * Return: pointer or NULL
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{
-	void *result;
 
-	if (new_size == old_size)
-		return (ptr);
-	if (new_size == 0 && ptr)
-	{
-		free(ptr);
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int index = 0;
+	char *ptr = NULL;
+
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	}
-	result = malloc(new_size);
-	if (result == NULL)
-		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
 	if (ptr == NULL)
-	{
-		fill_an_array(result, '\0', new_size);
-		free(ptr);
-	}
-	else
-	{
-		_memcpy(result, ptr, old_size);
-		free(ptr);
-	}
-	return (result);
-}
-/**
- * _memset - fills a memory with constant byte
- * @s: pointer to memory area
- * @n: first n bytes
- * @byt: constant byte
- *
- * Return: A pointer to a character
- */
-char *_memset(char *s, char byt, unsigned int n)
-{
-	unsigned int i;
+		return (NULL);
 
-	for (i = 0; i < n; i++)
-	{
-		s[i] = byt;
-	}
-	return (s);
-}
-/**
- * free_data - frees data
- * @data: the data structure
- *
- * Return: (Success) positive number
- * ------- (Fail) negative number
- */
-int free_data(sh_t *data)
-{
-	free(data->line);
-	data->line = NULL;
-	free(data->args);
-	data->args = NULL;
-	free(data->cmd);
-	data->cmd = NULL;
-	free(data->error_msg);
-	data->error_msg = NULL;
-	return (0);
-}
-/**
- * _memcpy - cpies memory area
- * @dest: Destination memory area
- * @src: Source memory area
- * @n: Amount of memory byte
- *
- * Return: A pointer to dest
- */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int i;
+	for (; index < (nmemb * size); index++)
+		ptr[index] = 0;
 
-	for (i = 0; i < n; i++)
-	{
-		dest[i] = src[i];
-	}
-	return (dest);
+	return (ptr);
 }
